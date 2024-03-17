1e46ffbaa0261e1b3e9fbceebfa567ab71a16489
Ivan Mermelstein <ivan.mermelstein@gmail.com>
2024-03-17 17:11:01 -0300

    link mock

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,17 @@
+import Link from "next/link";
+
 const Button = () => {
   return (
     <button
       className="bg-slate-900 w-full py-2 rounded-lg cursor-pointer 
         hover:bg-yellow-200 hover:text-slate-900 transition ease-in-out delay-75 duration-100"
     >
-      Button
+      <Link
+        href={'https://ar.linkedin.com/in/ivanmermelstein'}
+        target="_blank"
+      >
+        Button
+      </Link>
     </button>
   );
 };
